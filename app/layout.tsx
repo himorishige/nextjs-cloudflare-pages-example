a5f2c8387869a969b653ad24692f2fec2acefef4
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-11-25 15:34:22 +0900

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import "./globals.css";
+import "../styles/globals.css";
 import Head from "./head";
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,30 @@
-import { createClient } from "microcms-js-sdk";
+import { FC } from "react";
+import styles from "../styles/Home.module.css";
 
-// revalidate = 0で従来のgetServerSidePropsと同じような動作、SSRに
-// revalidate = 1以上の数値を渡すことで従来のISRとして動作する
-// revalidateなしの場合は従来のgetStaticPropsと同じような動作、SSGとして
-export const revalidate = 0;
+const Home = async (): Promise<ReturnType<FC>> => {
+  const uuid = await fetch("https://uuid.rocks/plain").then(
+    async (response) => await response.text()
+  );
 
-const client = createClient({
-  serviceDomain: "iblab",
-  apiKey: "0b0927f153f6477db78828cb111b5edbf107",
-});
-
-async function getData() {
-  const data = await client.get({
-    endpoint: "blog",
-  });
-  return data.contents;
-}
-
-export default async function Home() {
-  const data = await getData();
   return (
-    <div>
-      {data.map((item: any) => {
-        return (
-          <div key={item.id}>
-            <h1>{item.title}</h1>
-          </div>
-        );
-      })}
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing{" "}
+          <code className={styles.code}>pages/index.tsx</code>
+        </p>
+
+        <p className={styles.description}>
+          Here&apos;s a server-side UUID:
+          <code className={styles.code}>{uuid}</code>
+        </p>
+      </main>
     </div>
   );
-}
+};
+
+export default Home;
