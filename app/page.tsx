3f48603892f58609f0a4d28935caa84a378fc484
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-11-25 15:43:00 +0900

    add: microcms-sdk

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
 import { FC } from "react";
 import styles from "../styles/Home.module.css";
+import { createClient } from "microcms-js-sdk";
+
+const client = createClient({
+  serviceDomain: "your-service-domain",
+  apiKey: "your-api-key",
+});
+
+async function getData() {
+  const data = await client.get({
+    endpoint: "blog",
+  });
+  return data.contents;
+}
 
 const Home = async (): Promise<ReturnType<FC>> => {
   const uuid = await fetch("https://uuid.rocks/plain").then(
     async (response) => await response.text()
   );
+
+  const data = await getData();
 
   return (
     <div className={styles.container}>
@@ -13,10 +28,15 @@ const Home = async (): Promise<ReturnType<FC>> => {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
+        <div>
+          {data.map((item: any) => {
+            return (
+              <div key={item.id}>
+                <h1>{item.title}</h1>
+              </div>
+            );
+          })}
+        </div>
 
         <p className={styles.description}>
           Here&apos;s a server-side UUID:
